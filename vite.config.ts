2bc51afd718f8d07660a44e82777e5177264fb31
oct-sky-out <kms3335k@naver.com>
2024-01-26 00:47:08 +0900

    add vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  ssr: {
-    noExternal: [
-      // MUI needs to be pre-processed by Vite in production: https://github.com/brillout/vite-plugin-ssr/discussions/901
-      "@mui/base",
-      "@mui/icons-material",
-      "@mui/material",
-      "@mui/utils",
-      "@mui/x-date-pickers", // これを追加
-    ],
-  },
+	plugins: [react(), vercel()],
+	ssr: {
+		noExternal: [
+			// MUI needs to be pre-processed by Vite in production: https://github.com/brillout/vite-plugin-ssr/discussions/901
+			'@mui/base',
+			'@mui/icons-material',
+			'@mui/material',
+			'@mui/utils',
+			'@mui/x-date-pickers', // これを追加
+		],
+	},
 });
