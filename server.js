c5e0cdb0958dbff30d7383ee35c6070ba382e959
oct-sky-out <kms3335k@naver.com>
2024-01-26 02:35:27 +0900

    edit

--- server.js
+++ server.js
@@ -1,74 +1,75 @@
 /* eslint-disable no-undef */
-import fs from "fs";
-import path from "path";
-import express from "express";
-import { createServer as createViteServer } from "vite";
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import { createServer as createViteServer } from 'vite';
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 const Port = process.env.PORT || 3000;
-const Base = process.env.BASE || "/";
+const Base = process.env.BASE || '/';
 
 const templateHtml = isProduction
-  ? fs.readFileSync("./dist/client/index.html", "utf-8")
-  : "";
+	? fs.readFileSync('./dist/client/index.html', 'utf-8')
+	: '';
 
 const ssrManifest = isProduction
-  ? await fs.readFile("./dist/client/ssr-manifest.json", "utf-8")
-  : undefined;
+	? await fs.readFileSync('./dist/client/.vite/ssr-manifest.json', 'utf-8')
+	: undefined;
 
 const app = express();
 let vite;
 
 // ? Add vite or respective production middlewares
 if (!isProduction) {
-  vite = await createViteServer({
-    server: { middlewareMode: true },
-    appType: "custom",
-  });
+	vite = await createViteServer({
+		server: { middlewareMode: true },
+		appType: 'custom',
+	});
 
-  app.use(vite.middlewares);
+	app.use(vite.middlewares);
 } else {
-  const sirv = (await import("sirv")).default;
-  const compression = (await import("compression")).default;
-  app.use(compression());
-  app.use(Base, sirv("./dist/client", {
-    extensions: [],
-    gzip: true,
-  }));
+	const sirv = (await import('sirv')).default;
+	const compression = (await import('compression')).default;
+	app.use(compression());
+	app.use(
+		Base,
+		sirv('./dist/client', {
+			extensions: [],
+			gzip: true,
+		})
+	);
 }
 
-app.use("*", async (req, res, next) => {
+app.use('*', async (req, res, next) => {
+	// ! Favicon Fix
+	if (req.originalUrl === '/favicon.ico') {
+		return res.sendFile(path.resolve('./public/vite.svg'));
+	}
 
-  // ! Favicon Fix
-  if (req.originalUrl === "/favicon.ico") {
-    return res.sendFile(path.resolve("./public/vite.svg"));
-  }
+	// ! SSR Render - Do not Edit if you don't know what heare whats going on
+	let template, render;
 
+	try {
+		if (!isProduction) {
+			template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
+			template = await vite.transformIndexHtml(req.originalUrl, template);
+			render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
+		} else {
+			template = templateHtml;
+			render = (await import('./dist/server/entry-server.js')).render;
+		}
 
-  // ! SSR Render - Do not Edit if you don't know what heare whats going on
-  let template, render;
+		const rendered = await render({ path: req.originalUrl }, ssrManifest);
+		const html = template.replace(`<!--app-html-->`, rendered ?? '');
 
-  try {
-    if (!isProduction) {
-      template = fs.readFileSync(path.resolve("./index.html"), "utf-8");
-      template = await vite.transformIndexHtml(req.originalUrl, template);
-      render = (await vite.ssrLoadModule("/src/entry-server.tsx")).render;
-    } else {
-      template = templateHtml;
-      render = (await import("./dist/server/entry-server.js")).render;
-    }
-
-    const rendered = await render({ path: req.originalUrl }, ssrManifest);
-    const html = template.replace(`<!--app-html-->`, rendered ?? '');
-
-    res.status(200).setHeader("Content-Type", "text/html").end(html);
-  } catch (error) {
-    vite.ssrFixStacktrace(error);
-    next(error);
-  }
+		res.status(200).setHeader('Content-Type', 'text/html').end(html);
+	} catch (error) {
+		vite.ssrFixStacktrace(error);
+		next(error);
+	}
 });
 
 // ? Start http server
 app.listen(Port, () => {
-  console.log(`Server running on http://localhost:${Port}`);
+	console.log(`Server running on http://localhost:${Port}`);
 });
